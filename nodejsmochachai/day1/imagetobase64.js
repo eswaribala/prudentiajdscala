38c750b19eee7f71bfaf43e60f2d150ebe9ed916
eswaribala <parameswaribala@gmail.com>
2021-12-22 13:09:28 +0530

    updated

--- nodejsmochachai/day1/imagetobase64.js
+++ nodejsmochachai/day1/imagetobase64.js
@@ -7,8 +7,23 @@ function image_base64(file){
        {
        data= new Buffer(imgContent).toString('base64')
         console.log(data)}
+        base64_image('copy.jpg',data)
+
        })
 }
+
+function base64_image(fileName,base64Data){
+    var bitmap=new Buffer(base64Data,'base64')
+    fs.writeFile(fileName,bitmap,function(err,result){
+        if(err)
+            console.log('Error occurred....',err)
+        else
+            console.log('Image created')
+    })
+
+
+}
+
 
 //convert image to base 64
 base64Data=image_base64('./assets/dentalhealth.png')
