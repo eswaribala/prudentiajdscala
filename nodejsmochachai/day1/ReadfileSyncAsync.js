4cead2aa4e0ec68572f13d578702b17e8c9e48e1
eswaribala <parameswaribala@gmail.com>
2021-12-22 14:41:58 +0530

    updated

--- nodejsmochachai/day1/ReadfileSyncAsync.js
+++ nodejsmochachai/day1/ReadfileSyncAsync.js
@@ -1,14 +1,16 @@
 fs=require('fs')
 
 //asynchronous
+console.log("Asynchronous Started.....")
 fs.readFile('./assets/data.txt',function(err,data){
     if(err)
        console.log(err)
     else {
         console.log("Asynchonous Data Reading....",data.toString())
     }
-})
 
+})
+console.log("Asynchronous Ended.....")
 //synchronous
 var response = fs.readFileSync("./assets/data.txt")
 console.log("Synchonous Started.......",response.toString())
