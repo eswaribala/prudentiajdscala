74952f0d7584b62881cff3cf51fa6d5a07126ffc
eswaribala <parameswaribala@gmail.com>
2021-12-22 14:23:32 +0530

    updated

---
+++ nodejsmochachai/day1/mongodbconntest.js
@@ -0,0 +1,16 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/customerdb";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    //insert data
+    var dbo = db.db("customerdb");
+    var myobj = { name: "John Dhere", address: "Pune" };
+    dbo.collection("customers").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+    });
+
+});
