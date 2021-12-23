87a62d9183a4cc5bc2210e2a8ba6b3ef870ccbf1
eswaribala <parameswaribala@gmail.com>
2021-12-23 14:50:27 +0530

    updated

--- nodejsmochachai/day2/test/asyncandawait/indextest.js
+++ nodejsmochachai/day2/test/asyncandawait/indextest.js
@@ -9,5 +9,16 @@ describe('Url should return 200 response',function(){
         //console.log(response.status);
          assert.ok(response.data.length==10)
     })
+
+    it("User\'s Email Pattern",async function(){
+        //increase the default timeout to 5000ms
+        this.timeout(5000)
+        const response=  await getUsers('https://jsonplaceholder.typicode.com/users')
+        //console.log(response.status);
+        assert.match(response.data[0].email,/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/,
+            'Email Pattern testing')
+    })
+
+
 })
 
