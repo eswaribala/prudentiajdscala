52685d7296e7485b608682b62f6b2183e95354bb
eswaribala <parameswaribala@gmail.com>
2021-12-23 11:05:08 +0530

    updated

---
+++ nodejsmochachai/day2/test/promise/indextest.js
@@ -0,0 +1,12 @@
+var mocha=require('mocha')
+var getUsers = require("../../src/asyncawait/index").getUsers
+assert=require('assert')
+describe('Url should return 200 response',function(){
+    it('Invoke getUsers method using axios',function(){
+//promise
+     return getUsers('https://jsonplaceholder.typicode.com/users').then(res=>{
+         assert.ok(res.data.length==10)
+     })
+
+    })
+})
