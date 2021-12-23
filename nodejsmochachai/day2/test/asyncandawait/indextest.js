72f133db2062410d923eeb4d6407aa75204bd9b5
eswaribala <parameswaribala@gmail.com>
2021-12-23 10:55:05 +0530

    updated

--- nodejsmochachai/day2/test/asyncandawait/indextest.js
+++ nodejsmochachai/day2/test/asyncandawait/indextest.js
@@ -1,9 +1,13 @@
 var mocha=require('mocha')
-const {getUsers} = require("../../src/asyncawait");
+var getUsers = require("../../src/asyncawait/index").getUsers
 assert=require('assert')
 describe('Url should return 200 response',function(){
     it('Invoke getUsers method using axios',async function(){
+        //increase the default timeout to 5000ms
+        this.timeout(5000)
          const response=  await getUsers('https://jsonplaceholder.typicode.com/users')
+        //console.log(response.status);
+         assert.ok(response.data.length==10)
     })
 })
 
