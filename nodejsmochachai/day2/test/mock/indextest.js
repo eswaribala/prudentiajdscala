32ce4bbf342eef5a2fccba25f47d0ca41e486341
eswaribala <parameswaribala@gmail.com>
2021-12-23 17:42:20 +0530

    updated

---
+++ nodejsmochachai/day2/test/mock/indextest.js
@@ -0,0 +1,100 @@
+const expect = require('chai').expect;
+const request = require('request');
+const sinon = require('sinon');
+const index = require('../../src/mock/index');
+
+describe('with mock: getUsersById', () => {
+    it('should  getUsersById', (done) => {
+       //step 1
+        let requestMock = sinon.mock(request);
+        const users = [
+            {
+                "id": 1,
+                "name": "Leanne Graham",
+                "username": "Bret",
+                "email": "Sincere@april.biz",
+                "address": {
+                    "street": "Kulas Light",
+                    "suite": "Apt. 556",
+                    "city": "Gwenborough",
+                    "zipcode": "92998-3874",
+                    "geo": {
+                        "lat": "-37.3159",
+                        "lng": "81.1496"
+                    }
+                },
+                "phone": "1-770-736-8031 x56442",
+                "website": "hildegard.org",
+                "company": {
+                    "name": "Romaguera-Crona",
+                    "catchPhrase": "Multi-layered client-server neural-net",
+                    "bs": "harness real-time e-markets"
+                }
+            },
+            {
+                "id": 2,
+                "name": "Ervin Howell",
+                "username": "Antonette",
+                "email": "Shanna@melissa.tv",
+                "address": {
+                    "street": "Victor Plains",
+                    "suite": "Suite 879",
+                    "city": "Wisokyburgh",
+                    "zipcode": "90566-7771",
+                    "geo": {
+                        "lat": "-43.9509",
+                        "lng": "-34.4618"
+                    }
+                },
+                "phone": "010-692-6593 x09125",
+                "website": "anastasia.net",
+                "company": {
+                    "name": "Deckow-Crist",
+                    "catchPhrase": "Proactive didactic contingency",
+                    "bs": "synergize scalable supply-chains"
+                }
+            },
+            {
+                "id": 3,
+                "name": "Clementine Bauch",
+                "username": "Samantha",
+                "email": "Nathan@yesenia.net",
+                "address": {
+                    "street": "Douglas Extension",
+                    "suite": "Suite 847",
+                    "city": "McKenziehaven",
+                    "zipcode": "59590-4157",
+                    "geo": {
+                        "lat": "-68.6102",
+                        "lng": "-47.0653"
+                    }
+                },
+                "phone": "1-463-123-4447",
+                "website": "ramiro.info",
+                "company": {
+                    "name": "Romaguera-Jacobson",
+                    "catchPhrase": "Face to face bifurcated interface",
+                    "bs": "e-enable strategic applications"
+                }
+            }];
+
+        //step 2 setup
+        requestMock.expects("get")
+            .once()
+            .withArgs('https://jsonplaceholder.typicode.com/users')
+            .yields(null, null, JSON.stringify(users));
+       //step 3 invocation
+        index.getUserById('https://jsonplaceholder.typicode.com/users').then((users) => {
+            expect(users.length).to.equal(3);
+            users.forEach((user) => {
+                expect(user).to.have.property('id');
+                expect(user).to.have.property('name');
+                expect(user).to.have.property('email');
+            });
+
+            requestMock.verify();
+            requestMock.restore();
+            done();
+        });
+    });
+});
