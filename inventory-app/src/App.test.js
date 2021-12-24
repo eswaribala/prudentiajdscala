19ee5bffa67588d55d42b4ed083663e8f6491d10
eswaribala <parameswaribala@gmail.com>
2021-12-24 17:05:50 +0530

    updated

--- inventory-app/src/App.test.js
+++ inventory-app/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+import { shallow } from 'enzyme';
+/*
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
+
+describe('Screen should have text as John Deere Inventory App',()=>{
+    it('Shallow component should have snapshots',()=>{
+        const wrapper=shallow(App)
+
+    })
+})
