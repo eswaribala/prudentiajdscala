f8ea3b86e1880680c45e24e7e8e1f4fa7a85a9b7
eswaribala <parameswaribala@gmail.com>
2021-12-27 15:37:54 +0530

    updated

--- inventory-app/src/fetchData/FetchData.js
+++ inventory-app/src/fetchData/FetchData.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 const apiUrl='http://localhost:4000/'
-export const FetchData= async query=>{
+export const FetchData= async ()=>{
         return await axios.get(apiUrl);
 }
+
