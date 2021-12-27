48c374d54ec0e48dbcc1945b3be8400955b12e03
eswaribala <parameswaribala@gmail.com>
2021-12-27 12:19:41 +0530

    updated

---
+++ nodejsmochachai/day1/controllers/productapi.js
@@ -0,0 +1,30 @@
+const bodyParser = require("body-parser");
+var express=require('express')
+var cors=require("cors");
+config=require('../assets/config')
+//customerService=require('../services/customerservice')
+app=express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//GET
+app.get('/',function(request,response){
+    response.send("Product API and Running.....")
+})
+
+app.post('/products',function(request,response){
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    response.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    response.setHeader('Access-Control-Allow-Credentials', true);
+    console.log(request.body);
+
+    //console.log(request);
+    response.end("Data Received....")
+})
+
+app.listen(config.port,function()
+{
+    console.log('Server started at....',config.port);
+});
